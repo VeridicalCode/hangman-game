01a4eb99da86a1fc9430ead3207db9e3dd7915ef
quething <m.getsoian@gmail.com>
2019-07-28 20:14:46 -0700

    basic structure and pseudocode

---
+++ assets/docs/gamescript.js
@@ -0,0 +1,18 @@
+
+
+// establish variables
+    // current word
+    // array: easy words
+    // array: difficult words
+
+// randomly generate a word from the appropriate array
+    // display the correct number of blank spaces
+
+// check for keyboard input (letter guess by player)
+    // store as a variable
+    // check against current word, one letter at a time
+      // replace appropriate blank spaces with letters
+        // if word is complete, alert player to win
+
+    // if guess fails, add a demerit (give the hang man a body part)
+      // if hangman's body is complete, alert player to loss
