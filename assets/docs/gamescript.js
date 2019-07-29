de2cb15d5962ac71cf21ebd18717a6995869f697
quething <m.getsoian@gmail.com>
2019-07-28 20:33:09 -0700

    word arrays

--- assets/docs/gamescript.js
+++ assets/docs/gamescript.js
@@ -1,9 +1,53 @@
 
 
 // establish variables
-    // current word
-    // array: easy words
-    // array: difficult words
+    var currentWord; // current word
+    var easyWords = [ // array: easy words
+      "airplane",
+      "already",
+      "baseball",
+      "bedtime",
+      "cherry",
+      "choose",
+      "cobweb",
+      "cracker",
+      "debate",
+      "dinner",
+      "dwell",
+      "early",
+      "elbow",
+      "flavor",
+      "fireman",
+      "goldfish"
+    ];
+    var hardWords = [ // array: difficult words
+      "anachronistic",
+      "adulation",
+      "adversity",
+      "benevolent",
+      "clairvoyant",
+      "collaborate",
+      "convergence",
+      "digression",
+      "enervate",
+      "ephemeral",
+      "exemplary",
+      "extenuating",
+      "hedonist",
+      "hypothesis",
+      "impetuous",
+      "inconsequential",
+      "jubilation",
+      "ostentatious",
+      "propsperity",
+      "proclivity",
+      "querulous",
+      "sagacity",
+      "spontaneity",
+      "superficial",
+      "transient",
+      "vindicate"
+    ] ;
 
 // randomly generate a word from the appropriate array
     // display the correct number of blank spaces
