d8377e42eb7aac99466490450b01ee904d4ec0d5
quething <m.getsoian@gmail.com>
2019-08-02 11:33:01 -0700

    set theme & created functioning word generator

--- assets/docs/gamescript.js
+++ assets/docs/gamescript.js
@@ -1,125 +1,78 @@
 
+/*
+  For consistency we're going to figure out all our variables & their variations right here.
+  currentWord: the random word for this game, chosen at start
+    currentWordKnown: a string of what the player knows (blanks & letters)
+    currentWordDiv: the div that displays all this
+  tentacleLetters: the number of bad letters (which generate tentacles)
+    tentacleLettersArray: a collection of those guesses
+    tentacleLettersDiv: displays bad guesses to the player
+
+*/
 
 // establish variables
 var currentWord; // current word
-var lettersRemaining;  // number of empty spaces remaining
-var unguessedArray; // array to display & track empty spaces
+var currentWordArray; // changes from _ _ _ to _ x _ as player makes guesses
+var tentacleLetters = 0; // at 8 the player gets eaten
+var tentacleLettersArray = []; // holds bad guesses for display
 var playerGuess; // current letter being guessed by player
-var badPicksArray; // incorrect letters guessed by player
-var missesRemaining; // number of fails left before loss
 var easyWords = [ // array: easy words
-  "airplane",
-  "already",
-  "baseball",
-  "bedtime",
-  "cherry",
-  "choose",
-  "cobweb",
-  "cracker",
-  "debate",
-  "dinner",
-  "dwell",
-  "early",
-  "elbow",
-  "flavor",
-  "fireman",
-  "goldfish",
-  "habit",
-  "imitate",
-  "intend",
-  "journey",
-  "locate",
-  "loyal",
-  "method",
-  "mystery",
-  "nectar",
-  "opposite",
+  "breakwater",
+  "coral",
+  "octopus",
   "predator",
-  "process",
-  "remark",
-  "reverse",
-  "signal",
-  "struggle",
-  "symbol",
   "treasure",
-  "triumph",
-  "vision",
-  "wisdom"
+  "hurricane",
+  "lighthouse",
+  "lobster", 
+  "dolphin",
+  "manatee",
+  "manta",
+  "jellyfish",
+  "turtle",
+  "turbulence",
+  "seahorse",
+  "starfish",
+  "undertow"
 ];
 var hardWords = [ // array: difficult words
-  "anachronistic",
-  "adulation",
-  "adversity",
-  "benevolent",
-  "clairvoyant",
-  "collaborate",
-  "convergence",
-  "digression",
-  "enervate",
-  "ephemeral",
-  "exemplary",
-  "extenuating",
-  "hedonist",
-  "hypothesis",
-  "impetuous",
-  "inconsequential",
-  "jubilation",
-  "ostentatious",
-  "propsperity",
-  "proclivity",
-  "querulous",
-  "sagacity",
-  "spontaneity",
-  "superficial",
-  "transient",
-  "vindicate"
+  "abyssal",
+  "anemone",
+  "baleen",
+  "bathysphere",
+  "bivalve",
+  "conch",
+  "echinoderm",
+  "egret",
+  "estuary",
+  "floresence",
+  "guyot",
+  "invertebrate",
+  "leviathan",
+  "mussels",
+  "pelagic",
+  "phytoplankton",
+  "tsunami",
+  "zooplankton"
 ];
 
 // randomly generate a word from the appropriate array
 pickRandomWord = function (diffSetting) {
-  if (diffSetting == "easy") {
+  currentWordArray = [];
+  if (diffSetting == 'easy') {
     currentWord = easyWords[Math.floor(Math.random() * easyWords.length)];
   }
   else {
     currentWord = hardWords[Math.floor(Math.random() * hardWords.length)];
-  }
+  };
   // display the correct number of blank spaces
   for (i = 0; i < currentWord.length; i++) {
-    unguessedArray[i] = "_"
-  }
-}
-// check for keyboard input (letter guess by player)
-document.onkeyup = function (event) {
-  // store as a variable
-  playerGuess = event.key;
-  // assume the guess was wrong
-  goodGuess = false;
-
-  // check against current word, one letter at a time
-  for (j = 0; j < currentWord.length; j++) {
-    if (playerGuess == currentWord[j]) {
-      // replace appropriate blank spaces with letters
-      unguessedArray[j] = currentWord[j];
-      // tell the function this was a good guess
-      goodGuess = true;
-      // iterate blank space count
-      lettersRemaining--;
-      if (lettersRemaining < 1) {
-        // if word is complete, alert player to win
-        alert("You win!");
-      }
-    }
-  }
-  // if we get to the end of the word and haven't matched the letter
-  if (j == currentWord.length && goodGuess === false) {
-    // add current guess to list of invalid letters
-    badPicksArray.push(playerGuess);
-    // iterate fail counter
-    missesRemaining++;
-    // if hangman's body is complete, alert player to loss
-    if (missesRemaining < 1) {
-      alert("You lose");
-    }
-  }
-}
+    currentWordArray[i] = "_"
+  };
+  console.log(currentWord + " " + currentWordArray);
+ }
+ 
+// $.each(unguessedArray, function(index, value){
+//   $('#displayGuessDiv').html( $('#displayGuessDiv').html() + " " + unguessedArray[i])
+// }
 
