655d497adb7b520f683f58d164af8c6fda3a52fa
quething <m.getsoian@gmail.com>
2019-08-03 23:06:47 -0700

    media query shenanigans

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # hangman-game
 
-"Thalassophobia," an ocean-themed javascript hangman demo. The player tries to guess the mystery word, one letter at a time. Each miss adds a tentacle to the Kraken, which consumes the user's little scuba-diving avatar once it reaches eight tentacles. If the user completes the word before the Kraken finishes reconstituting, the little scuba guy escapes.
-
-A few things set this apart from the baseline Hangman game:
-
-a) The ocean theme
-
-b) Multiple source word arrays, allowing the player to select different difficulties
-
-c) If the player figures out the word early, they can guess the full word insted of finishing off one letter at a time
+"Thalassophobia," an ocean-themed javascript hangman demo. The player tries to guess the mystery word, one letter at a time. Each miss adds a tentacle to the Kraken, which 'consumes' the player (currently no animation for this, just use your imagination). If the player completes the word before the Kraken finishes reconstituting, the game is won and the Kraken retreats into the deep.
--- assets/docs/gamescript.js
+++ assets/docs/gamescript.js
@@ -1,156 +1,159 @@
 
 
-// establish variables
-var activeGame = false; // tells the page when to start listening to keypresses
-var currentWord; // current word
-var currentWordArray; // changes from _ _ _ to _ x _ as player makes guesses
-var tentacleLetters; // at 8 the player gets eaten
-var tentacleLettersArray = [' ']; // holds bad guesses for display. leave the space there, it freaks out if it tries to concat with no content
-var lettersToWin; // how many blank spaces to fill before victory
-var easyWords = [ // array: easy words
-  "breakwater",
-  "coral",
-  "octopus",
-  "predator",
-  "treasure",
-  "hurricane",
-  "lighthouse",
-  "lobster",
-  "dolphin",
-  "manatee",
-  "manta",
-  "jellyfish",
-  "turtle",
-  "turbulence",
-  "seahorse",
-  "starfish",
-  "undertow"
-];
-var hardWords = [ // array: difficult words
-  "abyssal",
-  "anemone",
-  "baleen",
-  "bathysphere",
-  "bivalve",
-  "conch",
-  "echinoderm",
-  "egret",
-  "estuary",
-  "fluorescence",
-  "guyot",
-  "invertebrate",
-  "leviathan",
-  "mussels",
-  "pelagic",
-  "phytoplankton",
-  "tsunami",
-  "zooplankton"
-];
-var krakenArray = [ // kraken images, stored for ease of replacement
-  '<img src="./assets/images/kraken-00.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-01.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-02.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-03.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-04.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-05.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-06.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
-  '<img src="./assets/images/kraken-07.gif" alt="Behold, the Kraken!" class="img-fluid"/>'
-];
+$(document).ready(function () {
 
-// we'll be calling this in several places so just function it for convenience
-displayCurrentWord = function () {
-  // change the text content of selected divs to the stringed contents of each array
-  $('#currentWordDiv').text(currentWordArray.join(' '));
-  $('#tentacleLettersDiv').text(tentacleLettersArray.join(' '));
-  $('.wordbox').show();
-  // and log the thing to console for debugging purposes
-  console.log(currentWord + " " + currentWordArray.join(' '));
-  console.log(tentacleLetters + " " + tentacleLettersArray.join(' '));
-}
+  // establish variables
+  var activeGame = false; // tells the page when to start listening to keypresses
+  var currentWord; // current word
+  var currentWordArray; // changes from _ _ _ to _ x _ as player makes guesses
+  var tentacleLetters; // at 8 the player gets eaten
+  var tentacleLettersArray = [' ']; // holds bad guesses for display. leave the space there, it freaks out if it tries to concat with no content
+  var lettersToWin; // how many blank spaces to fill before victory
+  var easyWords = [ // array: easy words
+    "breakwater",
+    "octopus",
+    "predator",
+    "treasure",
+    "hurricane",
+    "lighthouse",
+    "lobster",
+    "dolphin",
+    "manatee",
+    "jellyfish",
+    "turtle",
+    "turbulence",
+    "seahorse",
+    "starfish",
+    "undertow"
+  ];
+  var hardWords = [ // array: difficult words
+    "abyssal",
+    "anemone",
+    "baleen",
+    "bathysphere",
+    "bivalve",
+    "conch",
+    "coral",
+    "echinoderm",
+    "egret",
+    "estuary",
+    "fluorescence",
+    "guyot",
+    "invertebrate",
+    "leviathan",
+    "manta",
+    "mussels",
+    "pelagic",
+    "phytoplankton",
+    "tsunami",
+    "zooplankton"
+  ];
+  var krakenArray = [ // kraken images, stored for ease of replacement
+    '<img src="./assets/images/kraken-00.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-01.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-02.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-03.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-04.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-05.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-06.gif" alt="Behold, the Kraken!" class="img-fluid"/>',
+    '<img src="./assets/images/kraken-07.gif" alt="Behold, the Kraken!" class="img-fluid"/>'
+  ];
 
-// for sanity, a function to make sure (keyboard) input is a letter
-function isLetter(txt) {
-  if (txt.toUpperCase() != txt.toLowerCase() && // not a number
-    txt[1] == undefined) {  // not longer than 1 character ('backspace', 'shift')
-    return true;
+  // we'll be calling this in several places so just function it for convenience
+  displayCurrentWord = function () {
+    // change the text content of selected divs to the stringed contents of each array
+    $('#currentWordDiv').text(currentWordArray.join(' '));
+    $('#tentacleLettersDiv').text(tentacleLettersArray.join(' '));
+    $('.wordbox').show();
+    // and log the thing to console for debugging purposes
+    console.log(currentWord + " " + currentWordArray.join(' '));
+    console.log(tentacleLetters + " " + tentacleLettersArray.join(' '));
   }
-  else {
-    return false;
-  }
-}
 
-// START GAME
-pickRandomWord = function (diffSetting) {
-  activeGame = true; // tell the page the game is active
-  currentWordArray = []; // zero out the display word
-  tentacleLettersArray = [' '] // zero out the bad guesses
-  tentacleLetters = 0; // zero out the fails
-  $('#krakenDiv').html(krakenArray[0]).show(); // reset the kraken
-  $('#buttonTrayDiv, #hideThisDiv').hide(); // hide the new game buttons
-
-  // pick the word based on the difficulty setting
-  if (diffSetting == 'easy') {
-    currentWord = easyWords[Math.floor(Math.random() * easyWords.length)];
-  }
-  else {
-    currentWord = hardWords[Math.floor(Math.random() * hardWords.length)];
-  };
-  // store the correct number of blank spaces
-  for (i = 0; i < currentWord.length; i++) {
-    currentWordArray[i] = "_"
-  };
-  // set the victory condition
-  lettersToWin = currentWord.length;
-  // finally, print the correct empty spaces to the screen
-  displayCurrentWord()
-}
-
-// okay here's the fun part. look for player keypress
-document.onkeyup = function (event) {
-  // store keyboard input as a variable
-  let playerGuess = event.key;
-  let goodGuess = false; // assume guess was wrong
-
-  // if the game's not running or the key's not a valid letter, stop here
-  if (!activeGame || !isLetter(playerGuess)) {
-    console.log('error check: active game status is ' + activeGame + " and isLetter returned " + isLetter(playerGuess));
-    return;
-  };
-
-  // now, for each letter in currentWord
-  for (j = 0; j < currentWord.length; j++) {
-    // force case for safety
-    if (playerGuess.toLowerCase() == currentWordArray[j]) {
-      return; // if the letter is valid but already guessed, do nothing
+  // for sanity, a function to make sure (keyboard) input is a letter
+  function isLetter(txt) {
+    if (txt.toUpperCase() != txt.toLowerCase() && // not a number
+      txt[1] == undefined) {  // not longer than 1 character ('backspace', 'shift')
+      return true;
     }
-    // if the pressed letter matches, change the guess array & iterate victory
-    if (playerGuess.toLowerCase() == currentWord[j]) {
-      currentWordArray[j] = playerGuess.toLowerCase();
-      goodGuess = true;
-      lettersToWin--;
+    else {
+      return false;
+    }
+  }
+
+  // START GAME
+  pickRandomWord = function (diffSetting) {
+    activeGame = true; // tell the page the game is active
+    currentWordArray = []; // zero out the display word
+    tentacleLettersArray = [' '] // zero out the bad guesses
+    tentacleLetters = 0; // zero out the fails
+    $('#krakenDiv').html(krakenArray[0]).show(); // reset the kraken
+    $('#buttonTrayDiv, #hideThisDiv').hide(); // hide the new game buttons
+
+    // pick the word based on the difficulty setting
+    if (diffSetting == 'easy') {
+      currentWord = easyWords[Math.floor(Math.random() * easyWords.length)];
+    }
+    else {
+      currentWord = hardWords[Math.floor(Math.random() * hardWords.length)];
     };
-  };
-  // if we didn't match any letters, iterate tentacles, add bad guess to array, grab new kraken image
-  if (j >= currentWord.length && !goodGuess) {
-    tentacleLetters++;
-    tentacleLettersArray.push(playerGuess.toUpperCase());
-    $('#krakenDiv').html(krakenArray[tentacleLetters]);
-  };
-  // now refresh the display
-  displayCurrentWord();
-
-  if (lettersToWin == 0) {
-    $('#krakenDiv').hide(6000);
-    $('#buttonTrayDiv, #hideThisDiv').show();
-    $('.wordbox').hide();
-    alert("You win!");
-    activeGame = false;
+    // store the correct number of blank spaces
+    for (i = 0; i < currentWord.length; i++) {
+      currentWordArray[i] = "_"
+    };
+    // set the victory condition
+    lettersToWin = currentWord.length;
+    // finally, print the correct empty spaces to the screen
+    displayCurrentWord()
   }
 
-  if (tentacleLetters >= 8) {
-    $('#buttonTrayDiv, #hideThisDiv').show();
-    $('.wordbox').hide();
-    alert("You lose. The word was " + currentWord);
-    activeGame = false;
+  // okay here's the fun part. look for player keypress
+  document.onkeyup = function (event) {
+    // store keyboard input as a variable
+    let playerGuess = event.key;
+    let goodGuess = false; // assume guess was wrong
+
+    // if the game's not running or the key's not a valid letter, stop here
+    if (!activeGame || !isLetter(playerGuess)) {
+      console.log('error check: active game status is ' + activeGame + " and isLetter returned " + isLetter(playerGuess));
+      return;
+    };
+
+    // now, for each letter in currentWord
+    for (j = 0; j < currentWord.length; j++) {
+      // force case for safety
+      if (playerGuess.toLowerCase() == currentWordArray[j]) {
+        return; // if the letter is valid but already guessed, do nothing
+      }
+      // if the pressed letter matches, change the guess array & iterate victory
+      if (playerGuess.toLowerCase() == currentWord[j]) {
+        currentWordArray[j] = playerGuess.toLowerCase();
+        goodGuess = true;
+        lettersToWin--;
+      };
+    };
+    // if we didn't match any letters, iterate tentacles, add bad guess to array, grab new kraken image
+    if (j >= currentWord.length && !goodGuess) {
+      tentacleLetters++;
+      tentacleLettersArray.push(playerGuess.toUpperCase());
+      $('#krakenDiv').html(krakenArray[tentacleLetters]);
+    };
+    // now refresh the display
+    displayCurrentWord();
+
+    if (lettersToWin == 0) {
+      $('#krakenDiv').hide(6000);
+      $('#buttonTrayDiv, #hideThisDiv').show();
+      $('.wordbox').hide();
+      alert("You win!");
+      activeGame = false;
+    }
+
+    if (tentacleLetters >= 8) {
+      $('#buttonTrayDiv, #hideThisDiv').show();
+      $('.wordbox').hide();
+      alert("You lose. The word was " + currentWord);
+      activeGame = false;
+    }
   }
-}
+});
